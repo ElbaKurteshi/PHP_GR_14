545e66abc020c5c1da5447e95fdeec7ea5d186b8
ValmiraLokaj <44643433+ValmiraLokaj@users.noreply.github.com>
2019-05-26 14:26:27 +0200

    Update AlbanianFood.php

--- AlbanianFood.php
+++ AlbanianFood.php
@@ -32,4 +32,12 @@
                     <p><cite>Flija</cite> also known as fli or flija, is a dish in Albanian cuisine and in Balkan cuisine generally. It consists of multiple crepe-like layers brushed with cream and served with sour cream. The name translates to "sacrifice".
                     March 18 is recognized as "Flia Day" in which families invite their relatives for preparing and eating Flia.[citation needed]
 					Flija requires very simple ingredients: flour, water, butter, yogurt and salt. The main ingredients (flour, water and salt) are mixed together until they become like pancake batter. Layers of batter are baked using a sač which is a spherical metal lid used for baking.
-                    </p>  
+                    </p> 
+	</div>
+					
+					
+<div class="Albanian">
+			       <h3>Byrek</h3>
+                   <img src="images\pite.jpg" width=400 height=200> 
+                  <p>
+                   Savoury dinner pies are a favourite in Albania.  And they are just as hearty as their stews and just as flavourful.  Try byrek, made with feta cheese cabbage, spinach, tomatoes, and meat all layered in a tasty filo pastry. Kungullur is filo pastry and pumpkin, butter, salt, or sugar.  And finally Kakllaasarem – a traditional food from Kosovo, it’s a layered pita without anything inside and covered with garlic and yogurt, served warm. It’s a typical lunch for the Albanians.

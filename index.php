42766a05b406569492f82501c4f7b33d14e6063c
blinerabytyqi <44600201+blinerabytyqi@users.noreply.github.com>
2019-05-26 13:32:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ session_start();
 		
 		<link rel="stylesheet" href="stili.css">
 		<style type="text/css">
-			 .ushqimet{
+.ushqimet{
 	font-size:1em;
 	font:bold;
 	margin-top: 71em;
-    margin-left: -12px;
+        margin-left: -12px;
 	float:left;
-    width: 280px;
-    background-color:rgba(224, 224, 209, 0.7);
+       width: 280px;
+       background-color:rgba(224, 224, 209, 0.7);
     border: 3px solid #f5f5f0;
 	border-radius: 8px;	
 			}

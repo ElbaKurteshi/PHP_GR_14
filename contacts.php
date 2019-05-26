2c37306e43d62a9beb8346cc6ece59815dd23f56
ElbaKurteshi <44671780+ElbaKurteshi@users.noreply.github.com>
2019-05-26 13:59:17 +0200

    Update contacts.php

--- contacts.php
+++ contacts.php
@@ -19,5 +19,23 @@ $(document).ready(function(){
 	
 	</head>
 <link rel="stylesheet" href="contacts.css">
+	
+	<body> 
+<header>
+<nav>
+<div id='active'>
+            <ul>
+            <li ><a href='index.php'><span>Home</span></a></li>
+            <li ><a href="ItalianFood.php"><span>Italian Food</span></a></li>
+            <li ><a href="mexican.php"><span>Mexican Food</span></a></li>
+            <li ><a href="chinese.php"><span>Chinese Food</span></a></li>
+            <li ><a href="american.php"><span>American Food</span></a></li>
+            <li ><a href="AlbanianFood.php"><span>Albanian Food</span></a></li>
+            <li ><a href="contacts.php"><span>Contacts</span></a></li>
+            <li ><a href="login.php"><span>Log In</span></a></li>
+            </ul>
+			</div>
+</nav>
+</header>
 
 

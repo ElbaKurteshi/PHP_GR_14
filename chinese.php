fa94084863a112e7f8e678da5d593b39fd537b53
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2019-05-26 14:49:44 -0800

    Create chinese.php

---
+++ chinese.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title> Chinese Food </title>
+<meta charset="utf-8">
+</head>
+<link rel="stylesheet" href="chinese.css">
+<style type="text/css">
+
+    .sorting {
+    font-size:1em;
+    font:bold;
+    margin-top:25em;
+   
+    

0e2199bc30f4e35d1f94f7438ab46e3a6d111a60
ElbaKurteshi <elbakurteshi99@gmail.com>
2019-05-26 13:50:29 +0200

    Create contacts.php

---
+++ contacts.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+<title> Contacts </title>
+<meta charset="utf-8">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("#hide").click(function(){
+        $("p").hide(1000);
+    });
+	
+	$("#show").click(function(){
+    $("p").show(1000);
+});
+});
+</script>
+

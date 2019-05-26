278161f0dd5d2b8a2f3926aaacc4e5f463c87b52
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2019-05-26 05:52:55 -0700

    Update chinese.php

--- chinese.php
+++ chinese.php
@@ -27,6 +27,36 @@
     background-color:rgba(255, 194, 102, 0.7);
     border-radius: 10px;    
     }
+ .numeric {
+    font-size:1em;
+    font:bold;
+    margin-top:5em;
+    float:left;
+    width: 230px;
+    background-color:rgba(255, 194, 102, 0.7);
+    border-radius: 10px;
+    }
+	
 
+
+</style>
+<body>
+<header>
+<nav>
+<div id='active'>
+            <ul>
+			<li ><a href='index.php'><span>Home</span></a></li>
+            <li ><a href="ItalianFood.php"><span>Italian Food</span></a></li>
+            <li ><a href="mexican.php"><span>Mexican Food</span></a></li>
+            <li ><a href="chinese.php"><span>Chinese Food</span></a></li>
+		    <li ><a href="american.php"><span>American Food</span></a></li>
+		    <li ><a href="AlbanianFood.php"><span>Albanian Food</span></a></li>
+			<li ><a href="contacts.php"><span>Contacts</span></a></li>
+            <li ><a href="login.php"><span>Log In</span></a></li>
+            </ul>
+			</div>
+</nav>
+</header>
+<section>
    
     

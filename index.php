219dde84366d4e7c461f6ff925c8e26a6a3fbd8c
blinerabytyqi <bytyqiblinera.20@gmail.com>
2019-05-26 13:20:34 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+
+<html manifest="manifest.appcache">
+    <head>
+        <title>Foods Blog</title>
+        <meta charset="utf-8">
+		
+		<link rel="stylesheet" href="stili.css">
+		<style type="text/css">
+			 .ushqimet{
+	font-size:1em;
+	font:bold;
+	margin-top: 71em;
+    margin-left: -12px;
+	float:left;
+    width: 280px;
+    background-color:rgba(224, 224, 209, 0.7);
+    border: 3px solid #f5f5f0;
+	border-radius: 8px;	
+			}
+
+			
+			
+			</head>
+			
+			
+			
+			</body>

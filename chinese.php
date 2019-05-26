c3cdfa49738eb70b071de72b2adf57405d43e556
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2019-05-26 05:52:28 -0700

    Update chinese.php

--- chinese.php
+++ chinese.php
@@ -12,5 +12,21 @@
     font-size:1em;
     font:bold;
     margin-top:25em;
+         float:left;
+    width: 230px;
+    background-color:rgba(255, 194, 102, 0.7);
+    border-radius: 10px;  
+    }
+
+    .numeric1 {
+    font-size:1em;
+    font:bold;
+    margin-top:5em;
+    float:left;
+    width: 230px;
+    background-color:rgba(255, 194, 102, 0.7);
+    border-radius: 10px;    
+    }
+
    
     

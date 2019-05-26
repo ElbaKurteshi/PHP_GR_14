707c402f7f66997eaf70b2c175e24b06f3bcc69d
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2019-05-26 14:41:18 -0800

    Create american.php

---
+++ american.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title> American foods  </title>
+<meta charset="utf-8">
+<style>
+	
+	body{
+   font-family: Verdana, Arial, sans-serif;
+     background: url(tss1.jpg);
+     background-attachment: fixed;
+    background-repeat: no-repeat;
+     background-position:100%;
+    }

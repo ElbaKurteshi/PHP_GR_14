2a0c97eaafcd995d2396cb4b40a341ea69815dd0
blinerabytyqi <44600201+blinerabytyqi@users.noreply.github.com>
2019-05-26 14:55:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ session_start();
 	margin-top: 71em;
         margin-left: -12px;
 	float:left;
-       width: 280px;
+        width: 280px;
        background-color:rgba(224, 224, 209, 0.7);
        border: 3px solid #f5f5f0;
 	border-radius: 8px;	

65d69462a653c8cc4f9a504bee85ef8720bf2a10
blinerabytyqi <44600201+blinerabytyqi@users.noreply.github.com>
2019-05-26 13:22:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,34 @@ session_start();
     border: 3px solid #f5f5f0;
 	border-radius: 8px;	
 			}
+.twitter{
+	font-size:1em;
+	font:bold;
+	margin-top: 23em;
+	float:left;
+    width: 280px;
+    background-color:rgba(224, 224, 209, 0.7);
+    }
 
+
+.all {
+  width: 251px;
+  height:75px;
+  font-size: 1em;
+  font-weight:bold;
+  background-color:rgba(255, 0, 0, 0.1);
+ 
+  position:relative;
+  margin-top:3em;
+  margin-bottom:1.5em;
+  padding-top:2em;
+  padding-bottom:0.2em;
+  text-align: center;
+  border:solid 1px #669999;
+}
+			
+			
+		</style>
 			
 			
 			</head>

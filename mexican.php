667f44eb74161dfc33ec3b1ab264db3788dc17d5
ElbaKurteshi <elbakurteshi99@gmail.com>
2019-05-30 15:05:57 +0200

    Create mexican.php

---
+++ mexican.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title> Mexican </title>
+<meta charset="utf-8">
+</head>
+<link rel="stylesheet" href="mexican.css">
+<body>
+<header>
+<nav>
+<div id='active'>
+            <ul>
+			<li ><a href='index.php'><span>Home</span></a></li>
+            <li ><a href="ItalianFood.php"><span>Italian Food</span></a></li>
+            <li ><a href="mexican.php"><span>Mexican Food</span></a></li>
+            <li ><a href="chinese.php"><span>Chinese Food</span></a></li>
+		    <li ><a href="american.php"><span>American Food</span></a></li>
+		    <li ><a href="AlbanianFood.php"><span>Albanian Food</span></a></li>
+			<li ><a href="contacts.php"><span>Contacts</span></a></li>
+			<li ><a href="login.php"><span>Log In</span></a></li>
+            </ul>
+			</div>
+</nav>
+</header>
+<hr>
+

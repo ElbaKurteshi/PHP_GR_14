6d8d220e6bace2df0e6addb25e39ba703858b6ed
ValmiraLokaj <44643433+ValmiraLokaj@users.noreply.github.com>
2019-05-30 20:41:56 +0200

    Update ItalianFood.php

--- ItalianFood.php
+++ ItalianFood.php
@@ -23,4 +23,22 @@ $(document).ready(function(){
     });
 });
 </script>
+    <body>
+<header>
+<nav>
+<div id='active'>
+            <ul>
+            <li ><a href='index.php'><span>Home</span></a></li>
+            <li ><a href="ItalianFood.php"><span>Italian Food</span></a></li>
+            <li ><a href="mexican.php"><span>Mexican Food</span></a></li>
+            <li ><a href="chinese.php"><span>Chinese Food</span></a></li>
+            <li ><a href="american.php"><span>American Food</span></a></li>
+            <li ><a href="AlbanianFood.php"><span>Albanian Food</span></a></li>
+            <li ><a href="contacts.php"><span>Contacts</span></a></li>
+            <li ><a href="login.php"><span>Log In</span></a></li>
+            </ul>
+            </div>
+</nav>
+</header>
+<hr>
 

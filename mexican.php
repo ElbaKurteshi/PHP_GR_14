bdb46686eabac832eaacfe31e07adcb6a0300d80
ElbaKurteshi <44671780+ElbaKurteshi@users.noreply.github.com>
2019-05-30 15:09:11 +0200

    Update mexican.php

--- mexican.php
+++ mexican.php
@@ -29,3 +29,8 @@
 <div class="Insects">
 <h3>Chilaquiles</h3>
 <img src="images\chilaquiles.jpg" width=400 height=200> 
+<p> This popular traditional breakfast dish features lightly fried corn tortillas cut into quarters and topped with green or red salsa (the red is slightly spicier). Scrambled or fried eggs and pulled chicken are usually added on top, as well as cheese and cream. Chilaquiles are often served with a healthy dose of frijoles (refried beans).
+Typically, corn tortillas cut in quarters and lightly fried are the basis of the dish.Green or red salsa or mole is poured over the crisp tortilla triangles. The mixture is simmered until the tortilla starts softening. Pulled chicken is sometimes added to the mix. It is commonly garnished with crema, crumbled queso fresco, raw onion rings and avocado slices. Chilaquiles can be served with refried beans, eggs (scrambled or fried), beef and guacamole as side dish.
+As with many Mexican dishes, regional and family variations are quite common. Usually, chilaquiles are eaten at breakfast or brunch. This makes them a popular recipe to use leftover tortillas and salsas.
+                    </p>    
+                    </div>

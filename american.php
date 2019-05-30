6d8a832e481f7b9e11b9d5257834388c09b503de
ElbaKurteshi <44671780+ElbaKurteshi@users.noreply.github.com>
2019-05-31 01:05:55 +0200

    Update american.php

--- american.php
+++ american.php
@@ -47,5 +47,23 @@ echo trim($str,"Scrapple aka Pork!");
 <br>
 </p>  
   </div>
+	
+	<script>
+function showHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "getHint.php?q=" + str, true);
+        xmlhttp.send();
+    }
+}
+</script>
 </body>
 </html>

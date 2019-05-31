405c44e3d7993d16214a97e03f09892e03f8d9cd
ElbaKurteshi <elbakurteshi99@gmail.com>
2019-05-31 11:22:18 +0200

    Create getHint.php

---
+++ getHint.php
@@ -0,0 +1,35 @@
+<?php
+// Array with names
+$a[] = "Alba";
+$a[] = "Blinera";
+$a[] = "Benxhellushja";
+$a[] = "Diana";
+$a[] = "Elba";
+$a[] = "Fiona";
+$a[] = "Fitore";
+$a[] = "Fjolla";
+$a[] = "Ira";
+$a[] = "Jonida";
+$a[] = "Kiara";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Orika";
+$a[] = "Pranvera";
+$a[] = "Rinesa";
+$a[] = "Qendresa";
+$a[] = "Valmira";
+$a[] = "Pajtesa";
+$a[] = "Edona";
+$a[] = "Ema";
+$a[] = "Sara";
+$a[] = "Tea";
+$a[] = "Uvejsa";
+$a[] = "Violeta";
+$a[] = "Liza";
+$a[] = "Elizabeta";
+$a[] = "Elda";
+$a[] = "Wendi";
+$a[] = "Valeza";
+$a[] = "Blenda";
+
+

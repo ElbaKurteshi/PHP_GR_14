5496bcaf3212fbbc9aea3bf539a8de3063724267
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2019-06-02 06:51:18 -0700

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title> Sign Up </title>
+<meta charset="utf-8">
+
+</head>
+<link rel="stylesheet" href="signup.css">
+
+<!--jQuery Events-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"> </script> 
+<script src="jquery-3.2.1.min.js"></script> 
+
+<body>
+<header>
+<nav>
+ <ul>
+            <li ><a href='index.php'><span>Home</span></a></li>
+            <li ><a href="ItalianFood.php"><span>Italian Food</span></a></li>
+            <li ><a href="mexican.php"><span>Mexican Food</span></a></li>
+            <li ><a href="chinese.php"><span>Chinese Food</span></a></li>
+            <li ><a href="american.php"><span>American Food</span></a></li>
+            <li ><a href="AlbanianFood.php"><span>Albanian Food</span></a></li>
+            <li ><a href="contacts.php"><span>Contacts</span></a></li>
+            <li ><a href="login.php"><span>Log In</span></a></li>
+            </ul>
+            
+</nav>
+</header>
